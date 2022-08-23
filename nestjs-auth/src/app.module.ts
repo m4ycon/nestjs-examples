65acb722eeba9e3cbb99e17ebe153e465a3e6c2d
Maycon Fabio <m.vinnicyus@gmail.com>
2022-08-22 22:14:13 -0300

    fix: Format with prettier

--- nestjs-auth/src/app.module.ts
+++ nestjs-auth/src/app.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
   imports: [PrismaModule],
--- nestjs-auth/src/prisma/prisma.module.ts
+++ nestjs-auth/src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma.service'
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
 })
 export class PrismaModule {}
