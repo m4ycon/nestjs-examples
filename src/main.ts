808b5a97d149ff53c1343bff5c067d66eaeed1a1
Maycon Fabio <m.vinnicyus@gmail.com>
2025-05-04 14:03:23 -0300

    feat: Add some default settings in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /* validator will strip validated (returned) object of any 
+      properties that do not use any validation decorators */
+      whitelist: true,
+      /* instead of stripping non-whitelisted properties 
+      validator will throw an exception */
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder().setTitle('NestJS Template').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
